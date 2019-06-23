52b4391f1fcf0e8b154b86a375bcb56e2ce34d78
Jeremy38100 <jeremyroche5@gmail.com>
2019-06-23 11:01:53 +0200

    Credentials README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 SpotifyCollabsNetwork
+
+# Credentials
+`private/api_credentials.json` :
+```{javascript}
+{
+  "id": "Client ID",
+  "secret": "Client Secret"
+}
+```
--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-var Spotify = require('node-spotify-api');
+const Spotify = require('node-spotify-api');
 const credentials = require('./private/api_credentials');
-console.log(credentials);
+const spotify = new Spotify(credentials);
 
-var spotify = new Spotify(credentials);
 
 // // spotify.search({ type: 'track', query: 'Qui dit mieux' }, function(err, data) {
 // //   if (err) {
 // //     return console.log('Error occurred: ' + err);
 // //   }
-
-// // console.log(data.tracks.items[0]);
-// // });
 
 async function request() {
   try {
